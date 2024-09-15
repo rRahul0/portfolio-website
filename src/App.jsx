7a38c824ef5d82d59362e179a029a1552fc3b0e2
rRahul0 <karmakarrahul503@gmail.com>
2024-09-15 09:43:31 +0530

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ function App() {
 
   return (
     <div className="overflow-x-hiddden text-stone-300 antialiased">
-      {/* <div className="fixed inset-0 -z-10">
+      <div className="fixed inset-0 -z-10">
         <div className="relative h-full w-full bg-black"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div><div className="absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]"></div></div>
-      </div> */}
+      </div>
 
       <div className="container w-10/12 mx-auto">
         <Navbar />
