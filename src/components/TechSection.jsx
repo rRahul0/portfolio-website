b812573335c4e4b70a48b2eceaf7fa1f0ef068ac
rRahul0 <karmakarrahul503@gmail.com>
2024-09-15 12:14:51 +0530

    Refactor TechSection component for better responsiveness and code organization

--- src/components/TechSection.jsx
+++ src/components/TechSection.jsx
@@ -3,6 +3,7 @@ import { RiReactjsLine } from "react-icons/ri";
 import { SiExpress, SiMongodb, SiMysql, SiTailwindcss } from "react-icons/si";
 import { motion } from "framer-motion";
 import { DiCss3, DiHtml5, DiJava, DiJavascript1, DiPython } from "react-icons/di";
+// import { tech } from "../data/info";
 
 const iconVariants = (duration) => ({
     initial: { y: -10 },
@@ -19,12 +20,12 @@ const iconVariants = (duration) => ({
 
 export default function TechSection() {
     return (
-        <div className="pb-24 flex flex-col items-center overflow-hidden"> {/* Removed unnecessary margins */}
+        <div className="pb-24 flex flex-col items-center ml-0 -mr-0 ">
             <motion.h2
                 whileInView={{ opacity: 1, y: 0 }}
                 initial={{ opacity: 0, y: -100 }}
                 transition={{ duration: 1.5 }}
-                className="text-3xl sm:text-4xl mb-12"
+                className="text-3xl sm:text-4xl mb-12 "
             >
                 Technologies
             </motion.h2>
@@ -33,7 +34,7 @@ export default function TechSection() {
                 whileInView={{ opacity: 1, y: 0 }}
                 initial={{ opacity: 0, x: -100 }}
                 transition={{ duration: 1.5 }}
-                className="flex flex-wrap items-center justify-center gap-6 sm:gap-8 ml-24 -mr-28" // Removed -mr-36
+                className="flex flex-wrap items-center justify-center gap-6 sm:gap-8 -mr-32 ml-20" // Adjusted gap for better responsiveness
             >
                 <motion.div initial="initial" animate="animate" variants={iconVariants(1.5)}>
                     <DiHtml5 className="text-5xl sm:text-7xl text-red-500" />
@@ -54,7 +55,7 @@ export default function TechSection() {
                 <motion.div initial="initial" animate="animate" variants={iconVariants(3)} >
                     <SiMongodb className="text-5xl sm:text-7xl text-green-400" />
                 </motion.div>
-                <motion.div initial="initial" animate="animate" variants={iconVariants(4.5)} >
+                <motion.div initial="initial" animate="animate" variants={iconVariants(5)} >
                     <SiExpress className="text-5xl sm:text-7xl text-gray-500" />
                 </motion.div>
                 <motion.div initial="initial" animate="animate" variants={iconVariants(2)} >
